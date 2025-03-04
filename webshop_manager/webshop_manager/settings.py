523d36db7288f3d5225622afa8b03ba5f6d70a20
caf9 <casper_frost@outlook.com>
2025-03-04 13:52:05 +0100

    removed rest_framework from installed apps

--- webshop_manager/webshop_manager/settings.py
+++ webshop_manager/webshop_manager/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+#    'rest_framework',
     'encrypted_model_fields',
     'shops.apps.ShopsConfig',  # Add your app
 ]
