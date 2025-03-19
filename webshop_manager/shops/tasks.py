560b4ad75f7685f997ac9a3002d666094a63ce06
Casper <casper_frost@outlook.com>
2025-03-19 21:23:23 +0100

    Update tasks.py

--- webshop_manager/shops/tasks.py
+++ webshop_manager/shops/tasks.py
@@ -204,6 +204,9 @@ def sync_to_shopify(shop, data, feed):
                     "price": str(i['variant']['price'])
                 }
             }
+            print(payload)
+            print("")
+            print(i)
             # url = f"https://{shop.shop_name}.myshopify.com/admin/api/2022-07/products.json"
             url = f"https://{shop.shop_name}.myshopify.com/admin/api/2022-07/products/" + str(i["variant"]["id"]) + ".json"
             # url = f"https://{shop.shop_name}.myshopify.com/admin/api/2022-07/products/" + str(product_id)+".json"
