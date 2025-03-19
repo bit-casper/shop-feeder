c215619219bb8e97c68af171d78a4402d80de466
Casper <casper_frost@outlook.com>
2025-03-19 20:09:36 +0100

    Update tasks.py

--- webshop_manager/shops/tasks.py
+++ webshop_manager/shops/tasks.py
@@ -90,11 +90,11 @@ def sync_to_shopify(shop, data, feed):
     with open('data.json', 'r') as f:
         for ishop in json.load(f):
             for ifeed in data:
-                if ishop['sku'] == ifeed['sku']:
-                    if ishop['price'] != ifeed['price']:
+                if ishop['variants']['sku'] == ifeed['sku']:
+                    if ishop['variants']['price'] != ifeed['price']:
                         changed_products.append({
                             "variant": {
-                                "id": ifeed['id'],
+                                "id": ishop['variants']['id'],
                                 "price": str(ifeed['price'])
                             }
                         })
