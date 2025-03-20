7faaf397d6a000a8549f878d8a24ddb90788652d
Casper <casper_frost@outlook.com>
2025-03-20 09:43:05 +0100

    Update tasks.py

--- webshop_manager/shops/tasks.py
+++ webshop_manager/shops/tasks.py
@@ -165,10 +165,12 @@ def sync_to_shopify(shop, data, feed):
             response = requests.put(url, json=payload, headers=headers)
 
             response.raise_for_status()
-            #product_id = response.json()['product']['id']
-            print(response.json())
-            SyncLog.objects.create(feed=feed, shop=shop, status='success', message=f"Product {product_id} synced")
-            #SyncLog.objects.create(feed=feed, shop=shop, status='success', message=f"{len(changed_products)} products were synced")
+            product_id = response.json()['variant']['product_id']
+            variant_id = response.json()['variant']['id']
+            # print(response.json())
+            # SyncLog.objects.create(feed=feed, shop=shop, status='success', message=f"Product {product_id} synced")
+            SyncLog.objects.create(feed=feed, shop=shop, status='success', message=f"Variant {variant_id} of product {product_id} synced")
+            # SyncLog.objects.create(feed=feed, shop=shop, status='success', message=f"{len(changed_products)} products were synced")
     except Exception as e:
         feed.sync_status = 'failed'
         feed.save()
