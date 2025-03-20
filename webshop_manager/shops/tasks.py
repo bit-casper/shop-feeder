a11ab022ddecf3079243e65f43b728ace988d60c
Casper <casper_frost@outlook.com>
2025-03-20 10:18:47 +0100

    Update tasks.py

--- webshop_manager/shops/tasks.py
+++ webshop_manager/shops/tasks.py
@@ -104,7 +104,8 @@ def sync_feed_to_shops(feed_id):
         # Now process all shops with the complete mapped_data
         for shop in feed.shops.all():
             if shop.shop_type == 'shopify':
-                sync_to_shopify(shop, mapped_data, feed)
+                # sync_to_shopify(shop, mapped_data, feed)
+                create_to_shopify(shop, mapped_data, feed)
             elif shop.shop_type == 'uniconta':
                 sync_to_uniconta(shop, mapped_data, feed)
 
