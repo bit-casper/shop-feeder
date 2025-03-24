92224601790d0bee4c760d6e185845f23de5c577
Casper <casper_frost@outlook.com>
2025-03-24 09:23:18 +0100

    Update tasks.py

--- webshop_manager/shops/tasks.py
+++ webshop_manager/shops/tasks.py
@@ -417,7 +417,7 @@ def sync_to_uniconta(shop, data, feed):
     
     # Execute the shopify update call
     try:
-        print(data)
+        print(data.text)
         # Loop over all changed products, build a payload for each of them and push it into shopify
         for i in data:
 
