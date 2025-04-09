39e53379b2a6aa374ad5ababd0f8e9922586fb00
Casper <casper_frost@outlook.com>
2025-04-09 10:59:31 +0200

    Update admin.py

--- webshop_manager/shops/admin.py
+++ webshop_manager/shops/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Client, Shop, Feed, SyncLog
+from .models import Client, Shop, Product, Feed, SyncLog
 
 admin.site.register(Client)
 admin.site.register(Shop)
+admin.site.register(Product)
 admin.site.register(Feed)
 admin.site.register(SyncLog)
